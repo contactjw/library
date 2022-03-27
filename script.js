d7dfe4c61b913762b8f4f4144519cd9dfd7cfe82
John West <contactjohnwest@gmail.com>
2022-03-27 11:48:42 -0700

    Add working submit and remove funcions

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
+let booksContainer = document.querySelector(".books-container");
+let addBookButton = document.querySelector(".add-book-button");
+let submitBookButton;
+let removeBookButton;
+
+
 let myLibrary = [];
 
 function Book() {
-
-}
-
-function addBookToLibrary() {
-
-}
-
-
-let booksContainer = document.querySelector(".books-container");
-let addBookButton = document.querySelector(".add-book-button");
-
-
-addBookButton.onclick = function(event) {
-
-    document.body.style.pointerEvents = "none";
 
     let newBookContainer = document.createElement("div");
     newBookContainer.setAttribute("class", "new-book-container");
@@ -32,39 +23,57 @@ addBookButton.onclick = function(event) {
     let titleInput = document.createElement("input");
     titleInput.setAttribute("type", "text");
     titleInput.setAttribute("placeholder", "title");
+    titleInput.required = true;
     form.appendChild(titleInput);
 
     let authorInput = document.createElement("input");
     authorInput.setAttribute("type", "text");
     authorInput.setAttribute("placeholder", "author");
+    authorInput.required = true;
     form.appendChild(authorInput);
 
     let pagesInput = document.createElement("input");
     pagesInput.setAttribute("type", "text");
     pagesInput.setAttribute("placeholder", "pages");
+    pagesInput.required = true;
     form.appendChild(pagesInput);
 
     let haveReadButton = document.createElement("button");
-    haveReadButton.setAttribute("class", "read");
+    haveReadButton.setAttribute("id", "read");
     haveReadButton.innerHTML="Read";
     form.appendChild(haveReadButton);
 
     let submitButton = document.createElement("button");
-    submitButton.setAttribute("class", "submit");
+    submitButton.setAttribute("id", "submit-book");
     submitButton.innerHTML="Submit";
     form.appendChild(submitButton);
 
     let removeButton = document.createElement("button");
-    removeButton.setAttribute("class", "remove");
+    removeButton.setAttribute("id", "remove");
     removeButton.innerHTML="Remove";
     form.appendChild(removeButton);
 
     booksContainer.appendChild(newBookContainer);
 
-    booksContainer.style.pointerEvents = "auto";
+    submitBookButton = document.querySelector(".submit-book");
+    removeBookButton = document.querySelector(".remove");
+    
+}
+
+function addBookToLibrary() {
 
 }
 
+
+addBookButton.addEventListener('click', function(event) {
+
+    document.body.style.pointerEvents = "none";
+
+    let newBook = new Book();
+    myLibrary.push(newBook);
+
+    booksContainer.style.pointerEvents = "auto";
+});
 
 document.addEventListener('click', function(event) {
     let book = booksContainer.lastChild;
@@ -73,7 +82,35 @@ document.addEventListener('click', function(event) {
   
     if (!isClickInside) {
         document.body.style.pointerEvents = "auto";
-        book.remove();
+        // book.remove();
     }
-})
+});
+
+
+// Weird way to remove elements parents, but it works!
+
+document.addEventListener('click',function(e){
+    if(e.target && e.target.id == 'remove'){
+        console.log("Hello");
+        let target = e.target;
+        let parent = e.target.parentElement;
+        let grandparent = parent.parentElement;
+        grandparent.remove();
+        //do something
+     }
+ });
+
+
+ document.addEventListener('click',function(e){
+    if(e.target && e.target.id == 'submit-book'){
+        let target = e.target;
+        let parent = e.target.parentElement;
+        let grandparent = parent.parentElement;
+        grandparent.style.boxShadow = 'none';
+        document.body.style.pointerEvents = "auto";
+        //do something
+     }
+ });
+
+
 
